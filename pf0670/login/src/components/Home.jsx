51a948f93ef79a9916bbbf30936ed49d6db521d3
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-10-20 09:53:40 -0300

    Usuário Login!

---
+++ pf0670/login/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+
+export default function Home() {
+  const verificar = sessionStorage.getItem("usuario-validado");
+
+  useEffect(() => {
+    if (verificar == null) {
+      window.location = "/";
+    }
+  }, []);
+
+  return (
+    <div>
+      Home
+      <p>Bem vindo usuário : {verificar}</p>
+    </div>
+  );
+}
