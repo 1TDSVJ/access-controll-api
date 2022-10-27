7658f41b7b84efefef1890ebc656823e62f9df12
RafaRamosCosta <rm94281@fiap.com.br>
2022-10-27 08:44:09 -0300

    Renomeei a com o meu rm

--- pf0670/ControleAcesso/src/br/com/fiap/dao/UsuarioDAO3.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.dao;
-
-import java.util.ArrayList;
-import java.util.List;
-import br.com.fiap.to.UsuarioTO;
-
-public class UsuarioDAO3 {
-
-}
---
+++ rm94281/ControleAcesso/src/br/com/fiap/dao/UsuarioDAO.java
@@ -0,0 +1,53 @@
+package br.com.fiap.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import br.com.fiap.to.UsuarioTO;
+
+public class UsuarioDAO {
+	public static List<UsuarioTO> listaUsuario;
+
+	public UsuarioDAO() {
+		if (listaUsuario == null) {
+			listaUsuario = new ArrayList<UsuarioTO>();
+			
+			UsuarioTO user = new UsuarioTO();
+			user.setLogin("Selva");
+			user.setSenha("12345");
+			listaUsuario.add(user);
+			
+			user = new UsuarioTO();
+			user.setLogin("2");
+			user.setSenha("2");
+			listaUsuario.add(user);
+			
+			user = new UsuarioTO();
+			user.setLogin("3");
+			user.setSenha("3");
+			listaUsuario.add(user);
+			
+			user = new UsuarioTO();
+			user.setLogin("4");
+			user.setSenha("4");
+			listaUsuario.add(user);
+			
+			user = new UsuarioTO();
+			user.setLogin("5");
+			user.setSenha("5");
+			listaUsuario.add(user);
+		}
+
+	}
+
+	public UsuarioTO loginDAO(UsuarioTO u) {
+		for (int i = 0; i < listaUsuario.size(); i++) {
+			if (listaUsuario.get(i).getLogin().equals(u.getLogin())
+					&& listaUsuario.get(i).getSenha().equals(u.getSenha())) {
+				System.out.println("USU�RIO : " + listaUsuario.get(i).getLogin() + " LOGOU!");
+				return listaUsuario.get(i);
+			}
+		}
+		System.out.println("USU�RIO : " + u.getLogin() + " N�O VALIDADO!");
+		return null;
+	}
+}
